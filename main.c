b0abf89f16022df660ac39953bb6965ed861f5f4
*k1l3r* <lucasgabrielmelo256@gmail.com>
2026-01-22 22:19:10 -0300

    main.c
    Game Over Adicionado.
    
    gameOver()

--- main.c
+++ main.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
+#include <locale.h>
+
+void gameOver() {
+    printf("----------------------------------------\n");
+    printf("|                                      |\n");
+    printf("|          G A M E   O V E R           |\n");
+    printf("|                                      |\n");
+    printf("|      Sua jornada termina aqui.       |\n");
+    printf("|                                      |\n");
+    printf("|        Pressione ENTER               |\n");
+    printf("|                                      |\n");
+    printf("----------------------------------------\n");
+    
+    // Função gameOver toda vez que quiser fazer o gameOver aparecer é so chamar a função
+    
+}
 
 int main() {
-    
-    printf("Projeto iniciado!\n");
+    setlocale(LC_ALL, "Portuguese"); // Permite Usar acentuação entre outros no printf
 
-    printf("ACABEI DE MUDAR AQUI SE LIGA MANO");
-    printf("wow");
-    
+    // quando perder
+    gameOver();
+    getchar(); // é um scanf para letras no noso caso para o ENTER
+
     return 0;
 }
